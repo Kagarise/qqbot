ed2e16530734d102470d9bf187dd28846c1aed9c
kagarise <3255385205@qq.com>
2021-05-02 21:12:45 +0800

    bot

--- src/plugins/pixiv/__init__.py
+++ src/plugins/pixiv/__init__.py
@@ -5,10 +5,12 @@ from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 from nonebot.adapters.cqhttp import Message
 
-from .data_source import p_search, get_illust_detail
+from .data_source import p_search, get_illust_info, p_rank, p_user
 
 pid = on_command("pid", rule=to_me(), priority=1)
 psearch = on_command("psearch", aliases={"ps"}, rule=to_me(), priority=1)
+prank = on_command("prank", aliases={"pr"}, rule=to_me(), priority=1)
+puser = on_command("puser", aliases={"pu"}, rule=to_me(), priority=1)
 
 
 @pid.handle()
@@ -21,16 +23,19 @@ async def handle_pid(bot: Bot, event: Event, state: T_State):
 
 @pid.got("id", prompt="请输入id")
 async def handle_pid_got(bot: Bot, event: Event, state: T_State):
-    id = state["id"]
     try:
-        img_url = await get_illust_detail(int(id))
+        id = state["id"]
+        data = await get_illust_info(int(id), url_only=False)
     except:
         await pid.send("格式：pid <id>")
         return
-    if img_url is None:
+    try:
+        await pid.send(f'''id:{data['id']}
+title:{data['title']}
+user_id:{data['user_id']}
+user_name:{data['user_name']}''' + Message(f'[CQ:image,file={data["url"]}]'))
+    except:
         await pid.send("诶嘿，搜索出错了")
-    else:
-        await pid.send(Message(f'[CQ:image,file={img_url}]'))
 
 
 @psearch.handle()
@@ -43,13 +48,60 @@ async def handle_ps(bot: Bot, event: Event, state: T_State):
 
 @psearch.got("key", prompt="请输入查询关键词")
 async def handle_ps_got(bot: Bot, event: Event, state: T_State):
-    key = state["key"]
     try:
-        img_id, img_url = await p_search(key)
+        key = state["key"]
+        data = await p_search(key)
+        await psearch.send(f'''id:{data['id']}
+title:{data['title']}
+user_id:{data['user_id']}
+user_name:{data['user_name']}''' + Message(f'[CQ:image,file={data["url"]}]'))
     except:
         await psearch.send("诶嘿，搜索出错了")
-        return
-    if img_id is None or img_url is None:
-        await psearch.send("诶嘿，搜索出错了")
-    else:
-        await psearch.send(Message(f'id:{img_id}[CQ:image,file={img_url}]'))
+
+
+@prank.handle()
+async def handle_pr(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).strip()
+    logger.success(f'prank: {event}\n')
+    if args:
+        state["rank"] = args
+
+
+@prank.got("rank", prompt="请输入排名（1-20）")
+async def handle_pr_got(bot: Bot, event: Event, state: T_State):
+    try:
+        rank = int(state["rank"]) - 1
+        if 0 < rank < 20:
+            data = await p_rank(rank)
+            await prank.send(f'''id:{data['id']}
+title:{data['title']}
+user_id:{data['user_id']}
+user_name:{data['user_name']}''' + Message(f'[CQ:image,file={data["url"]}]'))
+        else:
+            await prank.send("plz:\nrank∈[1,20] ∩ rank∈N*")
+    except:
+        await prank.send("诶嘿，搜索出错了")
+
+
+@puser.handle()
+async def handle_pu(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).strip()
+    logger.success(f'puser: {event}\n')
+    if args:
+        state["user_id"] = args
+
+
+@puser.got("user_id", prompt="请输入用户id")
+async def handle_pu_got(bot: Bot, event: Event, state: T_State):
+    try:
+        user_id = int(state["user_id"])
+        datas = await p_user(user_id)
+        if datas == []:
+            await puser.send("uid不存在或该uid无illust")
+            return
+        await puser.send(f'将展示{len(datas)}幅作品')
+        for data in datas:
+            await puser.send(f'''id:{data['id']}
+title:{data['title']}''' + Message(f'[CQ:image,file={data["url"]}]'))
+    except:
+        await puser.send("诶嘿，搜索出错了")
--- src/plugins/pixiv/data_source.py
+++ src/plugins/pixiv/data_source.py
@@ -6,21 +6,33 @@ from .config import Config
 
 
 # https://api.fczbl.vip/pixiv/v2.html
-async def get_illust_detail(id: int, mode="medium"):
+async def get_illust_info(id: int, mode="original", url_only=True):
     api = Config.search_api
     params = {
         'type': 'illust',
         'id': id
     }
     try:
-        data = httpx.get(api, params=params).json()
-        url = data['illust']['image_urls'][mode]
-        return f'{Config.pic_api}{url}'
+        data = httpx.get(api, params=params).json()['illust']
+        if data['meta_pages'] == []:
+            url = data['meta_single_page']['original_image_url']
+        else:
+            url = data['meta_pages'][0]['image_urls'][mode]
+        if url_only:
+            return f'{Config.pic_api}{url}'
+        else:
+            return {
+                'id': data['id'],
+                'title': data['title'],
+                'user_id': data['user']['id'],
+                'user_name': data['user']['name'],
+                'url': f'{Config.pic_api}{url}'
+            }
     except:
         return None
 
 
-async def p_search(key, mode="medium"):
+async def p_search(key):
     api = Config.search_api
     params = {
         'type': 'search',
@@ -29,11 +41,39 @@ async def p_search(key, mode="medium"):
     }
     try:
         data = httpx.get(api, params=params).json()['illusts']
-        # data.sort(key=lambda item: item['total_bookmarks'] / item['total_view'])
-        # data.reverse()
-        data = random.choice(data)
-        id = data['id']
-        url = data['image_urls'][mode]
-        return id, f'{Config.pic_api}{url}'
+        id = random.choice(data)['id']
+        data = await get_illust_info(id, url_only=False)
+        return data
     except:
-        return None, None
+        return None
+
+
+async def p_rank(rank, mode="day"):
+    api = Config.search_api
+    params = {
+        'type': 'rank',
+        'mode': mode
+    }
+    try:
+        data = httpx.get(api, params=params).json()['illusts']
+        id = data[rank]['id']
+        data = await get_illust_info(id, url_only=False)
+        return data
+    except:
+        return None
+
+
+async def p_user(user_id, limit=3):
+    api = Config.search_api
+    params = {
+        'type': 'member_illust',
+        'id': user_id
+    }
+    try:
+        data = httpx.get(api, params=params).json()['illusts']
+        datas = []
+        for idx in range(min(limit, len(data))):
+            datas.append(await get_illust_info(data[idx]['id'], url_only=False))
+        return datas
+    except:
+        return None
